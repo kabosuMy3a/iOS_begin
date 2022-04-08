80e5c5a0aeab9d32f8bb55bd46f86168f1c49308
kabosumy3a <my3a3380@gmail.com>
2022-04-08 21:16:03 +0900

    1

---
+++ swift_basic/playground.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+import SwiftUI
+/*
+for i in 0...99 {i}
+//safe, fast, expressive //
+let a: Int = 100 ;
+var b: Int = 200; b = 300;
+var d : Double = 2.9 ;
+var f : Float = 2.99;
+var ui : UInt = 0; print(ui);
+var flag : Bool = false ; flag = true ;
+var char : Character = "가";
+var s : String = "there is no \' in Swift, Use \"\nLDSF" ; print(s);
+
+var num = 10 ; type(of: num);
+*/
+
+
+var numbers: Array<Int> = Array<Int>() ;
+numbers.append(1) ;
+
